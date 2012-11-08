c0dc49f558f37b2fcd616dcd941e36db86ddf4c1
dpereyra <dpereyra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-08 20:19:54 +0000

    Fixed a minor issue with the maintenance title.
    git-svn-id: https://plugins.svn.wordpress.org/dp-maintenance-mode-lite/trunk@622679 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/dark/index.php
+++ templates/dark/index.php
@@ -29,7 +29,7 @@
     <meta name="description" content="<?php bloginfo('description'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title><? bloginfo( 'name' )?></title>	
+    <title><?php bloginfo( 'name' )?></title>	
 	
 	<!-- ########## CSS Files ########## -->
     	
