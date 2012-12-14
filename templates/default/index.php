7ffa97f1016c9a28bd4dd6957c16b25d55a938c4
dpereyra <dpereyra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-14 03:29:48 +0000

    Added a value to the translations
    git-svn-id: https://plugins.svn.wordpress.org/dp-maintenance-mode-lite/trunk@638961 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/default/index.php
+++ templates/default/index.php
@@ -276,7 +276,7 @@
                 
                 <div class="contactContent">
                 	<form onsubmit="$('#btn_contact').trigger('click'); return false;">
-                    	<input type="text" value="Your name" name="name" id="name" onfocus="if(this.value == '<?php _e("Your name")?>') { this.value = ''; }" onblur="if(this.value == '') { this.value = '<?php _e("Your name")?>'; }" class="text" />
+                    	<input type="text" value="<?php _e("Your name")?>" name="name" id="name" onfocus="if(this.value == '<?php _e("Your name")?>') { this.value = ''; }" onblur="if(this.value == '') { this.value = '<?php _e("Your name")?>'; }" class="text" />
                         
                         <input type="text" value="mail@mail.com" name="email" id="email" onfocus="if(this.value == 'mail@mail.com') { this.value = ''; }" onblur="if(this.value == '') { this.value = 'mail@mail.com'; }" class="text" />
                         
