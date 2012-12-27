8c5eba3bc698f422f77817825f3e5a77d45b5ce1
dpereyra <dpereyra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-27 20:12:44 +0000

    Fixed an issue in the timer widget
    git-svn-id: https://plugins.svn.wordpress.org/dp-maintenance-mode-lite/trunk@645103 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/dark/js/jquery.counterClock.js
+++ templates/dark/js/jquery.counterClock.js
@@ -18,7 +18,7 @@
 			opts[k] = opts[k] || defaults[k];
 		})
 		
-		launchDateFix = Date.UTC(opts.launchDate.year, (opts.launchDate.month - 1), opts.launchDate.day, opts.launchDate.hour, opts.launchDate.minute);
+		launchDateFix = new Date(opts.launchDate.year, (opts.launchDate.month - 1), opts.launchDate.day, opts.launchDate.hour, opts.launchDate.minute);
 		
 		gVars["launchDate"] = launchDateFix;
 
@@ -34,30 +34,64 @@
 		
 			var currentTime = new Date(), differenceTime;
 			
-			differenceTime = new Date(gVars.launchDate - currentTime.getTime());
-			
-			if(gVars.launchDate <= currentTime.getTime()) {
-				
-				var d = 0;
-				var h = 0;
-				var m = 0;
-				var s = 0;
-				
-			} else {
-			
-				var d = Math.floor(Math.abs((gVars.launchDate - currentTime.getTime()) / (24*60*60*1000)));
-				var h = differenceTime.getUTCHours();
-				var m = differenceTime.getUTCMinutes();
-				var s = differenceTime.getUTCSeconds();
-			
+			differenceTime = new Date(gVars.launchDate.getTime() - currentTime.getTime());
+
+			var d = Math.floor(Math.abs((gVars.launchDate.getTime() - currentTime.getTime()) / (24*60*60*1000)));
+			var h = differenceTime.getUTCHours();
+			var m = differenceTime.getUTCMinutes();
+			var s = differenceTime.getUTCSeconds();
+			if( differenceTime.getTime() < 0 ) {
+				d = 0;
+				h = 0;
+				m = 0;
+				s = 0;
 			}
+			//console.log(differenceTime.getTime());
+			$('.container_clock .days span').html(_str_pad(d, 2, "0", "STR_PAD_LEFT"));
+			$('.container_clock .hours span').html(_str_pad(h, 2, "0", "STR_PAD_LEFT"));
+			$('.container_clock .minutes span').html(_str_pad(m, 2, "0", "STR_PAD_LEFT"));
+			$('.container_clock .seconds span').html(_str_pad(s, 2, "0", "STR_PAD_LEFT"));
 			
-			$('.container_clock .days span').html(d);
-			$('.container_clock .hours span').html(h);
-			$('.container_clock .minutes span').html(m);
-			$('.container_clock .seconds span').html(s);
-			
+
 		},1000);
+	}
+	
+	function _str_pad(input, pad_length, pad_string, pad_type) {
+
+	  var half = '',
+		pad_to_go;
+	
+	  var str_pad_repeater = function (s, len) {
+		var collect = '',
+		  i;
+	
+		while (collect.length < len) {
+		  collect += s;
+		}
+		collect = collect.substr(0, len);
+	
+		return collect;
+	  };
+	
+	  input += '';
+	  pad_string = pad_string !== undefined ? pad_string : ' ';
+	
+	  if (pad_type != 'STR_PAD_LEFT' && pad_type != 'STR_PAD_RIGHT' && pad_type != 'STR_PAD_BOTH') {
+		pad_type = 'STR_PAD_RIGHT';
+	  }
+	  if ((pad_to_go = pad_length - input.length) > 0) {
+		if (pad_type == 'STR_PAD_LEFT') {
+		  input = str_pad_repeater(pad_string, pad_to_go) + input;
+		} else if (pad_type == 'STR_PAD_RIGHT') {
+		  input = input + str_pad_repeater(pad_string, pad_to_go);
+		} else if (pad_type == 'STR_PAD_BOTH') {
+		  half = str_pad_repeater(pad_string, Math.ceil(pad_to_go / 2));
+		  input = half + input + half;
+		  input = input.substr(0, pad_length);
+		}
+	  }
+	
+	  return input;
 	}
 	
 	
